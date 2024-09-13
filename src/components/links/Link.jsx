26a4468b96e0bc9835c7cd3830fd50cbbab2ad82
ryanluc7reis <ryan.dev18@outlook.com>
2024-09-12 22:06:07 -0300

    Implementando funçoes de redirect links

--- src/components/links/Link.jsx
+++ src/components/links/Link.jsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 import { useTheme } from "@/context/ContextTheme";
+import { isMobile } from "react-device-detect";
 
 import Image from "@/components/image/Image";
 
-const LinkContainer = styled.div`
+const LinkContainer = styled.a`
   width: 87%;
   height: 60px;
   background: ${(props) =>
@@ -42,8 +43,45 @@ const NameLink = styled.h4`
 export default function Link({ url, app }) {
   const { theme } = useTheme();
   const DarkCondition = theme === "dark" ? true : false;
+  const handleRedirect = () => {
+    // Verifica se está em dispositivo móvel
+    if (isMobile) {
+      const appDeepLink = getUniversalLink(app);
+
+      if (appDeepLink) {
+        // Tenta abrir o link do app
+        window.location.href = appDeepLink;
+
+        // Fallback após um tempo se o app não abrir
+        setTimeout(() => {
+          window.open(url, "_blank"); // Redireciona para a versão web em uma nova aba
+        }, 2000);
+      } else {
+        window.open(url, "_blank"); // Redireciona para a versão web se o deep link não for suportado, em nova aba
+      }
+    } else {
+      window.open(url, "_blank"); // Se não estiver no mobile, abre a versão web em uma nova aba
+    }
+  };
+
+  const getUniversalLink = (appName) => {
+    // Converte o nome do aplicativo para letras minúsculas
+    const lowerCaseAppName = appName.toLowerCase();
+
+    // Verifica o nome do aplicativo e retorna a URL universal correspondente
+    if (lowerCaseAppName === "Whatsapp") {
+      return "https://wa.me/5534998069617"; // URL universal do Facebook
+    } else if (lowerCaseAppName === "Instagram") {
+      return "https://www.instagram.com/ryanluc7reis"; // URL universal do Instagram
+    }
+    // Adicione outras URLs universais aqui
+    else {
+      return null; // Retorna null se não existir URL universal
+    }
+  };
+
   return (
-    <LinkContainer isDark={DarkCondition}>
+    <LinkContainer onClick={handleRedirect} isDark={DarkCondition}>
       {app === "Instagram" && <ImageByWidth image="instagram.png" alt="" />}
       {app === "Whatsapp" && <ImageByWidth image="whatsapp.png" alt="" />}
       {app === "Facebook" && <ImageByPadding image="facebook.png" alt="" />}
