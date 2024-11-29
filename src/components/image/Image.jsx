e710b3a2eb58a78d729a3231aa25d184e42e9e7d
ryanluc7reis <ryan.dev18@outlook.com>
2024-11-29 01:19:10 -0300

    Criado prop para retirar o backgorund color e cursor pointer de Image

--- src/components/image/Image.jsx
+++ src/components/image/Image.jsx
@@ -3,25 +3,38 @@ import styled from "styled-components";
 const StyledImage = styled.img`
   padding: 1px;
   border-radius: 15px;
-  cursor: pointer;
+  cursor: ${(props) => (props.noHover ? "default" : "pointer")};
+
   :active {
     background-color: #3a3a3b7a;
     transition: 0.5s;
   }
   @media (min-width: 768px) {
     :hover {
-      background-color: ${(props) =>
-        props.isDark ? props.theme.hoverDark : props.theme.hoverLight};
+      background: ${(props) => (props.noHover ? "transparent" : "#80808028")};
     }
   }
 `;
-export default function Image({ image, imageDark, isDark, isMenu, ...props }) {
+
+export default function Image({
+  image,
+  imageDark,
+  isDark,
+  isMenu,
+  noHover,
+  ...props
+}) {
   return (
     <>
       {isMenu ? (
         <StyledImage src={"/x.png"} {...props} />
       ) : (
-        <StyledImage src={isDark ? imageDark : image} {...props} />
+        <StyledImage
+          src={isDark ? imageDark : image}
+          isDark={isDark}
+          noHover={noHover}
+          {...props}
+        />
       )}
     </>
   );
